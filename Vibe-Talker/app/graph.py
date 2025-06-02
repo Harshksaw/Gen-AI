63f7c150a2232ec87190c097d51f1c61d7c20a47
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-06-02 01:50:23 +0000

    Refactor chatbot integration and update dependencies for improved performance

--- Vibe-Talker/app/graph.py
+++ Vibe-Talker/app/graph.py
@@ -1,49 +1,62 @@
+import os
 from typing import Annotated
-from typing_extensions import  TypedDict
-from langraph.graph.message import add_messages
-from langraph.graph.message import Message
-from langraph.graph.node import ToolNode
-from langraph.graph.state import StateGraph
-from langraph.graph.state import START, END
-from langraph.graph.state import tools_condition
-from langraph.llm import init_chat_model
-from langraph.llm import LLM
-from langraph.llm import init_tool_model
-from langraph.tool import tool
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import StateGraph, START, END
+from langchain_core.tools import tool
+from langchain.schema import SystemMessage
 
-llm = init_chat_model(
-    model_provider = "openai",
-    model = "gpt-4.1",
-)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-    
+
 @tool
-def run_command(cmd:str):
-    """Takes a command line prompt and excutes it on the user;s machine and return the output of the command.
-    Example: run_command(cmd="ls") where ls is the command to list the files"""
-   
-llm_with_tool = llm.bind_tools(tool=[run_command])  
-    
-def chatbot(state):
-    message = llm.invoke(state['messages'])
-    assert len(message.took_calls) <= 1
+def run_command(cmd: str):
+    """
+    Takes a command line prompt and executes it on the user's machine and 
+    returns the output of the command.
+    Example: run_command(cmd="ls") where ls is the command to list the files.
+    """
+    result = os.system(command=cmd)
+    return result
+
+llm = init_chat_model(
+    model_provider="openai", model="gpt-4.1"
+)
+llm_with_tool = llm.bind_tools(tools=[run_command])
+
+def chatbot(state: State):
+    system_prompt = SystemMessage(content="""
+        You are an AI Coding assistant who takes an input from user and based on available
+        tools you choose the correct tool and execute the commands.
+                                  
+        You can even execute commands and help user with the output of the command.
+
+        Always make sure to keep your generated codes and files in chat_gpt/ folder. you can create one if not already there.                           
+    """)
+
+    message = llm_with_tool.invoke([system_prompt] + state["messages"])
+    # assert len(message.tool_calls) <= 1
     return {"messages": [message]}
 
+tool_node = ToolNode(tools=[run_command])
 
+graph_builder = StateGraph(State)
 
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
 
-tool_node = ToolNode(tools = [])
-
-graph_builder =  StateGraph(State)
-
-graph_builder.add_node('chatbot', chatbot)
-graph_builder.add_node('tool_node', tool_node)
-
-graph_builder.add_edge(START, 'chatbot')
-graph_builder.add_conditional_edges('chatbot', tools_condition)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
 graph_builder.add_edge("tools", "chatbot")
-graph_builder.add_edge("chatbot",END)
+graph_builder.add_edge("chatbot", END)
 
-graph  = graph_builder.compile()
+graph = graph_builder.compile()
+
+def create_chat_graph(checkpointer):
+    return graph_builder.compile(checkpointer=checkpointer)
--- Vibe-Talker/app/main.py
+++ Vibe-Talker/app/main.py
@@ -1,41 +1,55 @@
+from dotenv import load_dotenv
 import speech_recognition as sr
-import os
 from langgraph.checkpoint.mongodb import MongoDBSaver
-MONOGODB_URI = "mongodb://admin:admin@localhost:27017/"
 from .graph import create_chat_graph
+import asyncio
+from openai.helpers import LocalAudioPlayer
+from openai import AsyncOpenAI
+
+load_dotenv()
+
+
+openai = AsyncOpenAI()
+
+MONGODB_URI = "mongodb://admin:admin@localhost:27017"
+config = {"configurable": {"thread_id": "7"}}
+ 
+
 def main():
-    # Initialize recognizer
-    
-    with MongoDBSaver.from_conn_string(MONOGODB_URI ) as checkpointer:
-        graph = create_chat_graph(checkpointer= checkpointer)
-        recognizer = sr.Recognizer()
+    with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
+        graph = create_chat_graph(checkpointer=checkpointer)
+        
+        
+        r = sr.Recognizer()
 
-        # Use the microphone as the audio source
         with sr.Microphone() as source:
-            print("Please speak something...")
-            # Adjust for ambient noise and record audio
-            recognizer.adjust_for_ambient_noise(source)
-            recognizer.pause_threshold = 4
-            
-            audio = recognizer.listen(source)
-            
-            sst = recognizer.recognize_google(audio)
-            print("You said: " + sst)
-            graph.stream
-            # Save the audio to a file
-            
-        
-        
-        
+            r.adjust_for_ambient_noise(source)
+            r.pause_threshold = 2
 
-    # # Recognize speech using Google Web Speech API
-    # try:
-    #     text = recognizer.recognize_google(audio)
-    #     print("You said: " + text)
-    # except sr.UnknownValueError:
-    #     print("Sorry, I could not understand the audio.")
-    # except sr.RequestError as e:
-    #     print(f"Could not request results; {e}")
-        
-        
+            while True:
+                print("Say something!")
+                audio = r.listen(source)
+
+                print("Processing audio...")
+                sst = r.recognize_google(audio)
+
+                print("You Said:", sst)
+                for event in graph.stream({ "messages": [{"role": "user", "content": sst}] }, config, stream_mode="values"):
+                    if "messages" in event:
+                            event["messages"][-1].pretty_print()
+
+
+async def speak(text: str):
+    async with openai.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="coral",
+        input=text,
+        instructions="Speak in a cheerful and positive tone.",
+        response_format="pcm",
+    ) as response:
+        await LocalAudioPlayer().play(response)
+
 main()
+
+# if __name__ == "__main__":
+#      asyncio.run(speak(text="This is a sample voice. Hi Piyush"))
